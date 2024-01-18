e7aa61f3a33a935e79586f901e81829fbdffc962
Nguyễn Xuân Hoàng <nguyenxuanhoang02042001@gmail.com>
2024-01-18 21:56:49 +0700

    Update AppException.java

--- src/main/java/com/github/hoangsvn/compoments/exceptions/AppException.java
+++ src/main/java/com/github/hoangsvn/compoments/exceptions/AppException.java
@@ -1,6 +1,7 @@
 package com.github.hoangsvn.compoments.exceptions;
 
 public class AppException extends RuntimeException {
+ 
     public AppException(String message){
         super(message);
     }
