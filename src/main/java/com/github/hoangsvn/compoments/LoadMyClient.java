f317035d1d87b05c407b254952b738066ce6d4ff
Nguyễn Xuân Hoàng <nguyenxuanhoangicloudvn@gmail.com>
2024-06-23 11:43:15 +0700

    rs

--- src/main/java/com/github/hoangsvn/compoments/LoadMyClient.java
+++ src/main/java/com/github/hoangsvn/compoments/LoadMyClient.java
@@ -1,7 +1,6 @@
 package com.github.hoangsvn.compoments;
 
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import com.github.hoangsvn.compoments.client.LoadClient;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.*;
--- src/main/java/com/github/hoangsvn/compoments/client/LoadClient.java
+++ src/main/java/com/github/hoangsvn/compoments/client/LoadClient.java
@@ -1,4 +1,4 @@
-package com.github.hoangsvn.compoments;
+package com.github.hoangsvn.compoments.client;
 
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
